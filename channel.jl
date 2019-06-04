2fc45ac224d855dfed2561c935f6cf189b81d8ca
Pol <polxargayiferrer@gmail.com>
2019-06-04 20:27:34 +0200

    channel file --> introduces error in the data sending

---
+++ channel.jl
@@ -0,0 +1,14 @@
+#this file represents the channel, in which some loses may occur
+#lost packets (commands) will be simulated here
+
+function channel()
+    threshold = 0.2
+    x = rand()
+    if x >= threshold
+        #println(x)
+        return true
+    elseif x < threshold
+        #println(x)
+        return false
+    end
+end
